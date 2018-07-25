306e3e68165bb3e94f285c1bd5db0dda5f2973b5
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-07-25 21:50:12 +0200

    Poprawiony kod

--- src/Zgadnij.java
+++ src/Zgadnij.java
@@ -4,27 +4,20 @@ public class Zgadnij {
     public static void main(String[] args) {
         int poczatek = 100;
         int koniec = 200;
-        boolean exit = false;
         Scanner scan = new Scanner(System.in);
         System.out.println("Podaj liczbę");
         int numer = scan.nextInt();
-        while (!exit) {
+        while (numer < poczatek || numer > koniec || numer % 3 != 0) {
             if (numer < poczatek) {
                 System.out.println("Podana liczba jest za mała");
-                System.out.println("Podaj liczbę");
-                numer = scan.nextInt();
             } else if (numer > koniec) {
                 System.out.println("Podana liczba jest za duża");
-                System.out.println("Podaj liczbę");
-                numer = scan.nextInt();
             } else if (numer % 3 != 0) {
                 System.out.println("Podana liczba nie jest podzielna przez 3");
-                System.out.println("Podaj liczbę");
-                numer = scan.nextInt();
-            } else if (exit = true) {
-                System.out.println("Twoja liczba jest ok");
-
             }
+            System.out.println("Podaj liczbę");
+            numer = scan.nextInt();
         }
+        System.out.println("Twoja liczba jest ok");
     }
 }
